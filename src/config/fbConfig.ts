1879b0095a54ab27a2b18c93b5a1dc45c4b0a82c
rafi9898 <r-podraza@wp.pl>
2019-07-27 15:24:01 +0200

    Added profile image for users

--- src/config/fbConfig.ts
+++ src/config/fbConfig.ts
@@ -1,13 +1,14 @@
 import firebase from "firebase/app";
 import "firebase/firestore";
 import "firebase/auth";
+import "firebase/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBlmHdIhpByZCuQFvBaR7bTa6AhTPSnXNo",
   authDomain: "messenger-app-rafi9898.firebaseapp.com",
   databaseURL: "https://messenger-app-rafi9898.firebaseio.com",
   projectId: "messenger-app-rafi9898",
-  storageBucket: "",
+  storageBucket: "messenger-app-rafi9898.appspot.com",
   messagingSenderId: "330722596515",
   appId: "1:330722596515:web:c3e4cc79642d135d"
 };
--- src/store/actions/authActions.ts
+++ src/store/actions/authActions.ts
@@ -31,19 +31,40 @@ export const signUp = (newUser: any) => {
   return (dispatch: any, getState: any, { getFirebase, getFirestore }: any) => {
     const firebase = getFirebase();
     const firestore = getFirestore();
-
     firebase
       .auth()
       .createUserWithEmailAndPassword(newUser.email, newUser.password)
       .then((resp: any) => {
-        return firestore
-          .collection("users")
-          .doc(resp.user.uid)
-          .set({
-            firstName: newUser.firstName,
-            lastName: newUser.lastName,
-            initials: newUser.firstName[0] + newUser.lastName[0]
-          });
+        if (newUser.profileImage.length > 0) {
+          return firebase
+            .storage()
+            .ref()
+            .child(`avatars/${resp.user.uid}`)
+            .put(newUser.profileImage[0])
+            .then((snapshot: any) => {
+              snapshot.ref.getDownloadURL().then((url: any) => {
+                firestore
+                  .collection("users")
+                  .doc(resp.user.uid)
+                  .set({
+                    firstName: newUser.firstName,
+                    lastName: newUser.lastName,
+                    initials: newUser.firstName[0] + newUser.lastName[0],
+                    picture: url
+                  });
+              });
+            });
+        } else {
+          return firestore
+            .collection("users")
+            .doc(resp.user.uid)
+            .set({
+              firstName: newUser.firstName,
+              lastName: newUser.lastName,
+              initials: newUser.firstName[0] + newUser.lastName[0],
+              picture: null
+            });
+        }
       })
       .then(() => {
         dispatch({ type: "SIGNUP_SUCCESS" });
